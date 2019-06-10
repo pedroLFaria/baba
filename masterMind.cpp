62f8788309666301e50d9c6b174da88f5f5f2e69
pedroLFaria <pedro.luiz.fl@gmail.com>
2019-06-10 16:33:19 -0300

    Esqueleto do tabuleiro.

---
+++ masterMind.cpp
@@ -0,0 +1,28 @@
+class tabuleiro{
+private:
+    string cores[10] = ['amarelo','verde','azul','vermelho','roxo','laranja','marrom','rosa','lilas','violeta'];
+    string jogadas[][];
+    string resposta[][];
+    string ordem[];
+    int numeroCoresUtilizadas;
+    int tamanhoCodigo;
+    int numeroMaximoPalpites;
+    int numeroPalpitesAtual;
+    int dificuldades[] = [];
+    void setResposta(){
+
+    }
+public:
+    void setOrdemRandom(int tamnaho){
+
+    }
+    void setOrdemJogador(int tamanho){
+
+    }
+    string getResposta(){
+
+    }
+    void palpites(string jogada){
+
+    }
+};
